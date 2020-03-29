17007da87914629da50b7d7e2198f798089cb5ed
Shagen Madunts <shagen.madunts@mail.ru>
2020-03-29 17:53:57 +0300

    Задания по уроку 3

---
+++ SQL/Lesson_3/Vk_add_tables.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS city_location;
+CREATE TABLE city_location(
+	id SERIAL PRIMARY KEY,
+    city VARCHAR(255),
+    INDEX users_cityname_idx(city)
+);
+
+DROP TABLE IF EXISTS region_location;
+CREATE TABLE region_location(
+	id SERIAL PRIMARY KEY,
+    region VARCHAR(255),
+    INDEX users_region_idx(region)
+);
+
+DROP TABLE IF EXISTS location;
+CREATE TABLE location(
+	id SERIAL PRIMARY KEY,
+    city_id BIGINT unsigned NOT NULL,
+    region_id BIGINT unsigned NOT NULL,
+    user_id BIGINT UNSIGNED NOT NULL,
+    created_at DATETIME DEFAULT NOW(),
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (city_id) REFERENCES city_location(id),
+    FOREIGN KEY (region_id) REFERENCES region_location(id)
+	
+);
